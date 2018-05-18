eb4df0cd8646b97c0ab6880a9b9d0c43bf178ee8
Chris Cardle <cdcardle@gmail.com>
2018-05-18 13:43:57 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,7 +13,7 @@ class StudentsController < ApplicationController
 
   def create
     puts params
-    @post = Post.create(first_name: params[:first_name], last_name: params[:last_name])
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
   end
 
 end
