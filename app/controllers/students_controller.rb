819656dc3996ba3e07b390cb8d5e497c2f296972
Chris Cardle <cdcardle@gmail.com>
2018-05-18 13:44:22 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,7 +12,6 @@ class StudentsController < ApplicationController
   end
 
   def create
-    puts params
     @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
   end
 
